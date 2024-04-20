16c04c6ed60fb5bcafe476a5972d060b41f54353
Irina Meroi <irimeroi7@gmail.com>
2024-04-20 15:29:48 -0700

    first commit

---
+++ primer-entrega.py
@@ -0,0 +1,8 @@
+# Crear un programa que permita emular el registro y almacenamiento de usuarios en una base de datos.
+# Crear el programa utilizando el concepto de funciones, diccionarios, bucles y condicionales.
+
+# El formato de registro es: Nombre de usuario y Contraseña.
+# Utilizar una función para almacenar la información y otra función para mostrar la información.
+# Utilizar un diccionario para almacenar dicha información, con el par usuario-contraseña (clave-valor).
+# Utilizar otra función para el login de usuarios, comprobando que la contraseña coincida con el usuario.
+
